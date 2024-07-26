81ed7d3404f8e5a6769a8045e542a481066ccaa8
aaron-soto <aaron.m.soto1@gmail.com>
2024-07-26 08:08:24 -0700

    chore: Update PWA manifest with start URL and scope

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  dest: "public",
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
 });
 
 export default withPWA({
